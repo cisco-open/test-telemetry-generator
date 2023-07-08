4eccd2234887675c4abab6df6080e050c3a72562
Manpreet Singh <manprsi5@cisco.com>
2023-07-08 13:00:12 +0530

    Fix hexadecimal chars

--- src/test/java/io/opentelemetry/contrib/generator/telemetry/jel/CustomExpressions.java
+++ src/test/java/io/opentelemetry/contrib/generator/telemetry/jel/CustomExpressions.java
@@ -13,8 +13,7 @@ public class CustomExpressions {
     private static final ConcurrentMap<String, Integer> counterMaps = new ConcurrentHashMap<>();
     private static final ConcurrentMap<String, Boolean> boolMaps = new ConcurrentHashMap<>();
     private static final List<Character> hexChars = Arrays.asList('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z');
+            'a', 'b', 'c', 'd', 'e', 'f');
 
     public static int geometricWith2(String stateKey) {
         if (!counterMaps.containsKey(stateKey)) {
@@ -71,7 +70,7 @@ public class CustomExpressions {
     public static String randomHexadecimal(int length) {
         StringBuilder hexBuilder = new StringBuilder();
         for (int i=0; i<length; i++) {
-            hexBuilder.append(hexChars.get(RandomUtils.nextInt(0, 36)));
+            hexBuilder.append(hexChars.get(RandomUtils.nextInt(0, 16)));
         }
         return hexBuilder.toString();
     }
